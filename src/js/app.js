c8133c938ee928f1e079ed4c1416d88ea4761fcd
Shane Malachow <shanemalachow@gmail.com>
2017-11-05 08:41:18 -0500

    GOD DAMN CSS

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,48 @@
-// Your code goes here
-
-var latitude;
-var longitude;
-
 function setCoord(pos) {
     if (pos.coords !== undefined) {
         latitude = pos.coords.latitude;
         longitude = pos.coords.longitude;
 
-        // var mapOptions = {
-        //     center: new google.maps.LatLng(latitude, longitude),
-        //     zoom: 10,
-        //     mapTypeId: google.maps.MapTypeId.ROADMAP
-        // };
-        document.getElementById("coords").innerHTML = latitude + " " + longitude;
-        // map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        map.panTo({lat: latitude, lng: longitude});
+
+        var xhr = new XMLHttpRequest();
+        var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBkgq6skqj_3s0ME2aQHUmq6N_EJ4iADKs&location=" + latitude + "," + longitude + "&radius=1000&keyword=restaurant&maxprice=1";
+        xhr.open("GET", url, false);
+        xhr.send();
+
+        var text = xhr.responseText + "";
+        var data = JSON.parse(text);
+
+        console.log(data["results"].length);
+        restaurants = data["results"];
+        var list = document.getElementById("restaurants");
+        for (var x = 0; x < data["results"].length; x++) {
+            var place = data["results"][x];
+            console.log(place);
+            if (place["name"] !== undefined) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: place["geometry"]["location"]["lat"],
+                        lng: place["geometry"]["location"]["lng"]
+                    },
+                    map: map
+                });
+                foodMarkers.push(marker);
+                var li = document.createElement("li");
+                li.innerHTML = place["name"] + "<br>" + place["vicinity"];
+                list.appendChild(li);
+            }
+        }
+
     }
 }
 
+function navigate() {
+    gm.nav.setDestination(function () {
+        console.log("Success")
+    }, function () {
+        console.log("Error")
+    }, {address: restaurants[currentRest]["vicinity"]}, true)
+}
+
 gm.info.getCurrentPosition(setCoord, null, true);
-gm.info.watchPosition(setCoord, null, true);
